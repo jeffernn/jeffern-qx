284b2a7e4877efc78fcf6d29177b96effa4cf3c2
jeffernn <624670457@qq.com>
2025-07-23 22:36:52 +0800

    Emby客户端会员破解脚本
    Emby客户端会员破解脚本

--- 重写/Embypremiere/Embypremiere.js
+++ 重写/Embypremiere/Embypremiere.js
@@ -2,11 +2,8 @@
 
 ************************
 QuantumultX :
+Jeffern2025
 ************************
-
-[rewrite_remote]
-https://raw.githubusercontent.com/qiangxinglin/Emby/main/QuantumultX/emby.conf, tag=Emby Premiere, update-interval=86400, opt-parser=false, enabled=true
-
 */
 
 var url = $request.url;
