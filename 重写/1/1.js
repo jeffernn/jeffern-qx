d6b4159216878e4dfb83414b6ebacbe9e5acd804
jeffernn <624670457@qq.com>
2025-07-27 23:41:12 +0800

    Update 1.js

--- 重写/1/1.js
+++ 重写/1/1.js
@@ -3,21 +3,16 @@ var url = $request.url;
 
 const myStatus = "HTTP/1.1 200 OK";
 const myHeaders = {
-    "Host": "ikuuu.ch",
-    "Connection": "keep-alive",
-    "sec-ch-ua-platform": "macOS",
-    "X-Requested-With": "XMLHttpRequest",
-    "User-Agent:": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+    "Server": "nginx",
+    "Date": "2025 15:01:25 GMT"
+    "Content-Type": "text/html; charset=UTF-8",
+    "Transfer-Encoding:": "chunked",
+    "Connection:": "keep-alive"
     "Accept": "application/json, text/javascript, */*; q=0.01"
-    "sec-ch-ua": ""Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138""
-    "sec-ch-ua-mobile": "?0"
-    "Sec-Fetch-Site": "same-origin"
-    "Sec-Fetch-Mode": "cors"
-    "Sec-Fetch-Dest": "empty"
-    "Referer": "https://ikuuu.ch/user/shop"
-    "Accept-Encoding": "gzip, deflate, br, zstd"
-    "Accept-Language": "zh-CN,zh;q=0.9"
-    "Cookie": "lang=zh-cn; uid=3798409; email=jeffern1030%40gmail.com; key=316740d3332b984d344bd30659366dd160d0398889c57; ip=0f0f7b1c56670a7848fd177d1acf10a8; expire_in=1754232642"
+    "Vary": "Accept-Encoding"
+    "Strict-Transport-Security": "max-age=31536000"
+    "Strict-Transport-Security": "max-age=31536000"
+    "Content-Encoding": "gzip"
 };
 
 
