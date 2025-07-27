83262bc54be91756a65f5e036edd6c3e2322b6be
jeffernn <624670457@qq.com>
2025-07-27 23:20:55 +0800

    Update 1.js

--- 重写/1/1.js
+++ 重写/1/1.js
@@ -1,4 +1,19 @@
-{
-  "money" : "200.00",
-  "ret" : 1
-}
+
+var url = $request.url;
+
+const myStatus = "HTTP/1.1 200 OK";
+const myHeaders = {
+    "Crack": "KS",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Method": "*",
+    "Access-Control-Allow-Credentials": "true"
+};
+
+
+if (url.indexOf('/user/money') != -1) {
+    obj = {
+        "money": "200.00",
+        "ret": "1",
+    };
+
