ec6a8c2536e51281239711f10fc4460d63af74a1
jeffernn <624670457@qq.com>
2026-03-11 03:17:46 +0800

    Update quark_play.js

--- 重写/quark_play.js
+++ 重写/quark_play.js
@@ -1,23 +1,13 @@
 try {
-
     let obj = JSON.parse($response.body);
-
-    if (obj.data && obj.data.video_list) {
-
-        obj.data.video_list.forEach(v => {
-
-            v.member_right = "SUPER_VIP";
-            v.right = "SUPER_VIP";
-            v.accessable = true;
-
+    // 精准匹配该接口的返回结构，修改accessable
+    if (obj.data && obj.data.video_list && obj.data.video_list.length > 0) {
+        obj.data.video_list.forEach(item => {
+            item.accessable = true; // 核心修改：改为可访问
         });
-
     }
-
     $done({ body: JSON.stringify(obj) });
-
 } catch (e) {
-
+    // 解析失败时原样返回，避免请求报错
     $done({ body: $response.body });
-
 }
