89ae7f96a666b0f2d13081e9bae2e04959e4dd7d
jeffernn <624670457@qq.com>
2026-03-11 03:46:37 +0800

    Update QQuark.js

--- 重写/QQuark.js
+++ 重写/QQuark.js
@@ -2,7 +2,7 @@ let body = {
   "status": 200,
   "code": 0,
   "data": {
-    "member_type": "svip",
+    "member_type": "SUPER_VIP",
     "super_vip_exp_at": 253392455349000,
     "total_capacity": 6597069766656
   }
