851cfd418a66ada801c67c00cc62a99a64c08341
jeffernn <624670457@qq.com>
2026-03-11 01:47:28 +0800

    Update Quarkkkk.js

--- 重写/Quarkkkk.js
+++ 重写/Quarkkkk.js
@@ -1,11 +1,12 @@
-var obj = {
-  "status": 200,
-  "code": 0,
-  "data": {
-    "member_type": "SUPER_VIP",
-    "super_vip_exp_at": 253392455349000,
-    "total_capacity": 6597069766656
-  }
-};
+try {
+    let obj = JSON.parse($response.body);
 
-$done({body: JSON.stringify(obj)});
+    if (obj.data) {
+        obj.data.member_type = "SUPER_VIP";
+        obj.data.super_vip_exp_at = 253392455349000;
+    }
+
+    $done({ body: JSON.stringify(obj) });
+} catch (e) {
+    $done({ body: $response.body });
+}
