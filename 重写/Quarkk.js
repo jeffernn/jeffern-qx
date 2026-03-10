fab6f0bc35505338296819686ad96c1c019a593c
jeffernn <624670457@qq.com>
2026-03-11 01:23:41 +0800

    Update and rename Quarkk.js to Quarkkk.js

--- 重写/Quarkk.js
+++
@@ -1,7 +0,0 @@
-// Quantumult X 重写脚本：修改 Quark 云盘成员信息响应
-let body = $response.body;
-
-// 直接替换响应体为指定的 JSON
-body = '{"status":200,"code":0,"data":{"member_type":"SUPER_VIP","super_vip_exp_at":253392455349000,"total_capacity":6597069766656}}';
-
-$done({body});
