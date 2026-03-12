6fd9d7307c2a1caba00bb2e6057855ece7410e9c
jeffernn <624670457@qq.com>
2026-03-12 12:01:32 +0800

    Update quark.js

--- 重写/quark.js
+++ 重写/quark.js
@@ -1,47 +1,130 @@
-let body = $response.body;
-if (!body) $done({});
+**************************************
 
-let obj = JSON.parse(body);
-let d = obj.data;
+[rewrite_local]
+^https?:\/\/drive.*\.quark\.cn\/.+\/clouddrive\/(member.+|distribute\/detail.+|capacity\/growth\/info) url script-response-body https://raw.githubusercontent.com/jeffernn/jeffern-qx/refs/heads/main/%E9%87%8D%E5%86%99/quark.js
 
-// ==============================================
-// 只修改【两个JSON有差异的字段】
-// ==============================================
+[mitm]
+hostname = drive*.quark.cn
 
-// 1. 外层
-obj.timestamp = 1773234550;
-obj.metadata.range_size = 20485760;
-obj.metadata.server_cur_time = 1773234550837;
+*************************************/
 
-// 2. data 基础
-d.member_type = "SUPER_VIP";
-d.total_capacity = 6597090738176;
-d.secret_total_capacity = 6597090738176;
-d.use_capacity = 128784273002;
-d.secret_use_capacity = 128784273002;
-d.created_at = 1622760342000;
 
-// 3. 扩容构成
-d.extend_capacity_composition = {
-  sign_reward: 20971520
-};
+var body = $response.body;
+var chxm1023 = JSON.parse(body);
 
-// 4. member_info
-d.member_info.file_save_to_remains = -1;
-d.member_info.video_save_to_remains = -1;
-d.member_info.offline_download_remains = -1;
+const vipa = '/clouddrive/member';
+const vipb = '/clouddrive/distribute/detail';
+const vipc = '/clouddrive/capacity/growth/info';
 
-// 5. 新增字段（老账号才有）
-d.exp_at = 1775836800000;
-d.super_vip_exp_at = 253392455349000;
+if ($request.url.indexOf(vipa) != -1){
+  chxm1023.data = {
+    "member_type": "SUPER_VIP",
+    "image_backup": 1,
+    "deep_recycle_stat": {
+      "recycle_normal_serve_days": 10,
+      "recycle_pay_serve_days": 30,
+      "deep_recycle_serve_days": 180
+    },
+    "member_info": {
+      "video_save_to_uses": 0,
+      "video_save_to_remains": -1,
+      "file_save_to_remains": -1,
+      "offline_download_remains": -1,
+      "member_type_map": {
+        "MINI_VIP": {
+          "video_save_to_total": 10
+        }
+      }
+    },
+    "acc_status": 0,
+    "secret_use_capacity": 55042952572,
+    "super_vip_exp_at": 4092599349000,
+    "use_capacity": 55042952572,
+    "video_backup": 1,
+    "extend_capacity_composition": {},
+    "is_new_user": false,
+    "member_status": {
+      "Z_VIP": "UNPAID",
+      "VIP": "UNPAID",
+      "SUPER_VIP": "UNPAID",
+      "MINI_VIP": "UNPAID"
+    },
+    "secret_total_capacity": 109951162777600,
+    "subscribe_pay_channel_map": {},
+    "fr_subscribe_status_map": {},
+    "exp_at": 4092599349000,
+    "subscribe_status_map": {},
+    "total_capacity": 109951162777600
+  };
+}
 
-// 6. 删除新账号独有的多余字段（避免结构错误）
-if (d.trial_status_map) delete d.trial_status_map;
+if ($request.url.indexOf(vipb) != -1){
+  chxm1023.data = {
+    "last_id": 0,
+    "last_page": true,
+    "distribute_detail": [{
+      "title": "SVIP会员",
+      "product_category": "MEMBER",
+      "member_detail": {
+        "member_type": "SUPER_VIP",
+        "capacity": 109951162777600
+      },
+      "expired_at": 4092599349000
+    }]
+  };
+}
 
-// ==============================================
-// 【绝对不动】
-// is_new_user 保持原样
-// 其他所有没差异的字段全部保留原返回
-// ==============================================
+if ($request.url.indexOf(vipc) != -1){
+  chxm1023.data = {
+    "member_type": "SUPER_VIP",
+    "super_vip_exp_at": 4092599349000,
+    "use_capacity": 55029395707,
+    "cap_growth": {
+      "cur_total_cap": 0
+    },
+    "88VIP": true,
+    "member_status": {
+      "Z_VIP": "UNPAID",
+      "SUPER_VIP": "UNPAID",
+      "MINI_VIP": "UNPAID",
+      "VIP": "UNPAID"
+    },
+    "cap_sign": {
+      "sign_daily": false,
+      "sign_target": 7,
+      "sign_daily_reward": 104857600,
+      "sign_progress": 0,
+      "sign_rewards": [{
+        "name": "+100MB",
+        "reward_cap": 104857600
+      }, {
+        "name": "+200MB",
+        "highlight": "翻倍",
+        "reward_cap": 209715200
+      }, {
+        "name": "+100MB",
+        "reward_cap": 104857600
+      }, {
+        "name": "+100MB",
+        "reward_cap": 104857600
+      }, {
+        "name": "+100MB",
+        "reward_cap": 104857600
+      }, {
+        "name": "+100MB",
+        "reward_cap": 104857600
+      }, {
+        "name": "+1024MB",
+        "highlight": "翻十倍",
+        "reward_cap": 1073741824
+      }]
+    },
+    "cap_composition": {
+      "other": 0,
+      "member_own": 109951162777600
+    },
+    "total_capacity": 109951162777600
+  };
+}
 
-$done({ body: JSON.stringify(obj) });
+$done({body : JSON.stringify(chxm1023)});
