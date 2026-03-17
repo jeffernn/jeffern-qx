d5a08f22f558f7ab6d8a806806eb8cac00f78516
jeffernn <624670457@qq.com>
2026-03-18 03:40:18 +0800

    Create pandas.js

---
+++ 重写/pandas.js
@@ -0,0 +1,88 @@
+// 定时调用函数（但 f2 未定义）
+setInterval(function () {
+  f2();
+}, 4000);
+
+// 固定的 Cookie（BDUSS）
+const COOKIE = "bduss=khmSlF3YVhOWjFzR1Q1bmpxd3paVks1NlA5LWlwdFVzTURDV34xQ2dtb3JlT1pvSVFBQUFBJCQAAAAAAQAAAAEAAAAddbqcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACvrvmgr675oY";
+
+// 如果是请求阶段
+if (typeof $response === "undefined" || Object.keys($response).length === 0) {
+
+  // 拦截百度网盘下载接口
+  if ($request.url.includes("/rest/2.0/pcs/file") &&
+      $request.url.includes("method=locatedownload")) {
+
+    let headers = $request.headers;
+
+    if (headers) {
+      // 注入 Cookie
+      headers.Cookie = COOKIE;
+
+      $done({
+        headers: headers
+      });
+    } else {
+      $done({});
+    }
+
+  } else {
+    $done({});
+  }
+
+// 如果是响应阶段
+} else if ($request.url.includes("/membership/user")) {
+
+  // 伪造会员信息（SVIP）
+  $done({
+    body: JSON.stringify({
+      currenttime: 1573473597,
+      request_id: 7501873289383875000,
+      product_infos: [
+        {
+          product_id: "5310897792128633390",
+          end_time: 32493834549,
+          buy_time: "1417260485",
+          cluster: "offlinedl",
+          start_time: 1417260485,
+          detail_cluster: "offlinedl",
+          product_name: "gz_telecom_exp"
+        },
+        {
+          product_name: "svip2_nd",
+          product_description: "超级会员",
+          function_num: 0,
+          start_time: 1553702399,
+          buy_description: "",
+          buy_time: 1417260485,
+          product_id: "1",
+          auto_upgrade_to_svip: 0,
+          end_time: 32493834549,
+          cluster: "vip",
+          detail_cluster: "svip",
+          status: 0
+        }
+      ],
+      reminder: {
+        reminderWithContent: [],
+        advertiseContent: []
+      }
+    })
+  });
+
+// 修改用户信息
+} else if ($request.url.includes("/user/getinfo")) {
+
+  let data = JSON.parse($response.body);
+
+  data.records[0].nick_name = "Jeffern";
+  data.records[0].priority_name = "Jeffern";
+  data.records[0].avatar_url = "https://zhongdu.oss-cn-beijing.aliyuncs.com/app/20250723/17532551159065978.jpg";
+
+  $done({
+    body: JSON.stringify(data)
+  });
+
+} else {
+  $done({});
+}
